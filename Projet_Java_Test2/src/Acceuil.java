2ae42a49e3247fd2ead65a30502ba8fef6696ddb
amraniyoussef <youssefamrani9@gmail.com>
2014-10-19 23:52:30 +0200

    Ucef commit okey

--- Projet_Java_Test2/src/Acceuil.java
+++ Projet_Java_Test2/src/Acceuil.java
@@ -14,7 +14,7 @@ public class Acceuil {
 		System.out.println("Alors ???");
 		System.out.println("Ucef : Alors Quoi ???");
 		System.out.println("c bon oublie");
-
+		System.out.println("Ucef : Okeey");
 		
 
 
