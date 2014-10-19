4e349d0b50a444afd2210d59acbffb49ffd6beda
amraniyoussef <youssefamrani9@gmail.com>
2014-10-19 23:00:16 +0200

    Commit Acceuil

---
+++ Projet_Java_Test2/src/Acceuil.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+
+public class Acceuil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Date d = new Date();
+		System.out.println("Date : " + d.toString());
+
+	}
+
+}
