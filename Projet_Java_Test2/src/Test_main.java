88b3cf157441a447a36d57fb0f54a9a2ec52c800
amraniyoussef <youssefamrani9@gmail.com>
2014-10-19 21:07:04 +0200

    Dev 2 Commit

--- Projet_Java_Test2/src/Test_main.java
+++ Projet_Java_Test2/src/Test_main.java
@@ -5,8 +5,9 @@ public class Test_main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Salut guys");
-		System.out.println("Bonsoir");
+		System.out.println("Hamza !Salut guys");
+		System.out.println("Ucef : Bonsoir");
+		System.out.println("Dev2 : Merhba");
 	}
 
 }
